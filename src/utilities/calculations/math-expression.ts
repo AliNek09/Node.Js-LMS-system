aafe9c11bb363fa9bb2c6ee867e56ffcdeaec04e
Sojid Bahronov <2005sojid@gmail.com>
2025-01-21 16:11:15 +0500

    bug fix

--- src/utilities/calculations/math-expression.ts
+++ src/utilities/calculations/math-expression.ts
@@ -54,7 +54,7 @@ export class MathExpression
   static calculateScore(submittedAnswers: any[], problems: Problem[]): number {
     let totalCorrectFields = 0;
     let totalFields = 0;
-
+    let submittedProblemsLength = 0;
     // Process each submitted answer against the original problems (not transformed ones)
     for (const submittedAnswer of submittedAnswers) {
       // Find the corresponding problem from original problems
@@ -63,18 +63,25 @@ export class MathExpression
       if (!problem) {
         throw new AppException('Submission has invalid problemId')
       }
-
+      problems.splice(problems.indexOf(problem), 1);
       const correctAnswerFields = problem.answer.fields;
-
+      totalFields += correctAnswerFields.length;
+      submittedProblemsLength++;
       for (const submittedField of submittedAnswer.fields) {
         const correctField = correctAnswerFields.find((f) => f.index === submittedField.index);
 
         if (correctField) {
-          totalFields++;
+
           if (this.compareAnswers(correctField.value, submittedField.value)) {
             totalCorrectFields++;
           }
         }
+      }
+    }
+
+    if (submittedProblemsLength > problems.length) {
+      for(const element of problems) {
+        totalFields += element.answer.fields.length;
       }
     }
 
